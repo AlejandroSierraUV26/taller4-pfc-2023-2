16f7db044bd4c37ee910ede9c844ff31b4847b7e
AlejandroSierraUV26 <104778436+AlejandroSierraUV26@users.noreply.github.com>
2023-12-04 14:12:09 -0500

    Test I

--- app/src/main/scala/taller4/Operacion.scala
+++ app/src/main/scala/taller4/Operacion.scala
@@ -1,6 +1,7 @@
 package taller4
 import common.task
 import taller4.Benchmark._
+
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.DurationInt
 import scala.concurrent.{Await, Future}
@@ -79,21 +80,24 @@ object Operacion {
     Vector.tabulate(m1.length, m1.length)((i, j) => m1(i)(j) - m2(i)(j))
   }
   def multStrassen(m1: Matriz, m2: Matriz): Matriz = {
-    // Recibe m1 y m2 matrices cuadradas de la misma dimensión (potencia de 2)
-    // y devuelve la multiplicación de las dos matrices usando el algoritmo de Strassen
-    val n = m1.head.count(_=> true)
+
+    val n = m1.head.count(_ => true)
+
     if (n == 1) {
       Vector(Vector(m1(0)(0) * m2(0)(0)))
     } else {
       val m = n / 2
+
       val a11 = subMatriz(m1, 0, 0, m)
       val a12 = subMatriz(m1, 0, m, m)
       val a21 = subMatriz(m1, m, 0, m)
       val a22 = subMatriz(m1, m, m, m)
+
       val b11 = subMatriz(m2, 0, 0, m)
       val b12 = subMatriz(m2, 0, m, m)
       val b21 = subMatriz(m2, m, 0, m)
       val b22 = subMatriz(m2, m, m, m)
+
       val p1 = multStrassen(sumMatriz(a11, a22), sumMatriz(b11, b22))
       val p2 = multStrassen(sumMatriz(a21, a22), b11)
       val p3 = multStrassen(a11, restarMatriz(b12, b22))
@@ -101,11 +105,13 @@ object Operacion {
       val p5 = multStrassen(sumMatriz(a11, a12), b22)
       val p6 = multStrassen(restarMatriz(a21, a11), sumMatriz(b11, b12))
       val p7 = multStrassen(restarMatriz(a12, a22), sumMatriz(b21, b22))
+
       val c11 = restarMatriz(sumMatriz(sumMatriz(p1, p4), p7), p5)
       val c12 = sumMatriz(p3, p5)
       val c21 = sumMatriz(p2, p4)
-      val c22 = restarMatriz(sumMatriz(sumMatriz(p1,p3),p6), p2)
+      val c22 = restarMatriz(sumMatriz(sumMatriz(p1, p3), p6), p2)
 
+      // Construir la matriz resultante
       Vector.tabulate(n, n) { (i, j) =>
         if (i < m && j < m) c11(i)(j)
         else if (i < m && j >= m) c12(i)(j - m)
@@ -114,48 +120,63 @@ object Operacion {
       }
     }
   }
+
   def multStrassenPar(m1: Matriz, m2: Matriz): Matriz = {
+
     val n = m1.head.count(_ => true)
+
     if (n == 1) {
       Vector(Vector(m1(0)(0) * m2(0)(0)))
     } else {
       val m = n / 2
-      val a11 = subMatriz(m1, 0, 0, m)
-      val a12 = subMatriz(m1, 0, m, m)
-      val a21 = subMatriz(m1, m, 0, m)
-      val a22 = subMatriz(m1, m, m, m)
-      val b11 = subMatriz(m2, 0, 0, m)
-      val b12 = subMatriz(m2, 0, m, m)
-      val b21 = subMatriz(m2, m, 0, m)
-      val b22 = subMatriz(m2, m, m, m)
-      val p1 = multStrassen(sumMatriz(a11, a22), sumMatriz(b11, b22))
-      val p2 = multStrassen(sumMatriz(a21, a22), b11)
-      val p3 = multStrassen(a11, restarMatriz(b12, b22))
-      val p4 = multStrassen(a22, restarMatriz(b21, b11))
-      val p5 = multStrassen(sumMatriz(a11, a12), b22)
-      val p6 = multStrassen(restarMatriz(a21, a11), sumMatriz(b11, b12))
-      val p7 = multStrassen(restarMatriz(a12, a22), sumMatriz(b21, b22))
-      val c11 = restarMatriz(sumMatriz(sumMatriz(p1, p4), p7), p5)
-      val c12 = sumMatriz(p3, p5)
-      val c21 = sumMatriz(p2, p4)
-      val c22 = restarMatriz(sumMatriz(sumMatriz(p1, p3), p6), p2)
 
+      val a11 = task {subMatriz(m1, 0, 0, m)}
+      val a12 = task {subMatriz(m1, 0, m, m)}
+      val a21 = task {subMatriz(m1, m, 0, m)}
+      val a22 = task {subMatriz(m1, m, m, m)}
+
+      val b11 = task {subMatriz(m2, 0, 0, m)}
+      val b12 = task {subMatriz(m2, 0, m, m)}
+      val b21 = task {subMatriz(m2, m, 0, m)}
+      val b22 = task {subMatriz(m2, m, m, m)}
+
+
+      val p1 = task {multStrassenPar(sumMatriz(a11.join, a22.join), sumMatriz(b11.join, b22.join))}
+      val p2 = task {multStrassenPar(sumMatriz(a21.join, a22.join), b11.join)}
+      val p3 = task {multStrassenPar(a11.join, restarMatriz(b12.join, b22.join))}
+      val p4 = task {multStrassenPar(a22.join, restarMatriz(b21.join, b11.join))}
+      val p5 = task {multStrassenPar(sumMatriz(a11.join, a12.join), b22.join)}
+      val p6 = task {multStrassenPar(restarMatriz(a21.join, a11.join), sumMatriz(b11.join, b12.join))}
+      val p7 = task {multStrassenPar(restarMatriz(a12.join, a22.join), sumMatriz(b21.join, b22.join))}
+
+      val c11 = restarMatriz(sumMatriz(sumMatriz(p1.join, p4.join), p7.join), p5.join)
+      val c12 = sumMatriz(p3.join, p5.join)
+      val c21 = sumMatriz(p2.join, p4.join)
+      val c22 = restarMatriz(sumMatriz(sumMatriz(p1.join, p3.join), p6.join), p2.join)
+
+
+
+      // Construir la matriz resultante
       Vector.tabulate(n, n) { (i, j) =>
-        task(
-          if (i < m && j < m) c11(i)(j)
-          else if (i < m && j >= m) c12(i)(j - m)
-          else if (i >= m && j < m) c21(i - m)(j)
-          else c22(i - m)(j - m)).join()
+        if (i < m && j < m) c11(i)(j)
+        else if (i < m && j >= m) c12(i)(j - m)
+        else if (i >= m && j < m) c21(i - m)(j)
+        else c22(i - m)(j - m)
       }
-    }
-  }
-
+    }}
   def main(args: Array[String]): Unit = {
+    /*
   for {
     i<- 1 to 8
     m1 = matrizAlAzar(math.pow(2,i).toInt, 2)
     m2 = matrizAlAzar(math.pow(2,i).toInt, 2)
   }yield (println(compararAlgoritmos(multMatrizRec, multMatrizRecPar)(m1, m2), math.pow(2,i).toInt))
+     */
+    val m1 = matrizAlAzar(4,4)
+    val m2 = matrizAlAzar(4,4)
+    println(multMatriz(m1,m2))
+    println(multStrassen(m1,m2))
+
   }
 }
 
--- app/src/test/scala/taller4/TestTaller4.scala
+++ app/src/test/scala/taller4/TestTaller4.scala
@@ -12,9 +12,44 @@ import org.scalatestplus.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class TestTaller4 extends AnyFunSuite{
-    test("testTaller4 I"){
+    test("testTaller4 I") {
+        val m1 = Vector(
+            Vector(1, 2),
+            Vector(3, 4))
+        val m2 = Vector(
+            Vector(5, 6),
+            Vector(7, 8))
+        assert(Operacion.multMatriz(m1, m2) == Vector(Vector(19, 22), Vector(43, 50)))
+        assert(Operacion.multMatrizPar(m1, m2) == Vector(Vector(19, 22), Vector(43, 50)))
+        assert(Operacion.multStrassen(m1, m2) == Vector(Vector(19, 22), Vector(43, 50)))
+        assert(Operacion.multStrassenPar(m1, m2) == Vector(Vector(19, 22), Vector(43, 50)))
+        assert(Operacion.multMatrizRec(m1, m2) == Vector(Vector(19, 22), Vector(43, 50)))
+        assert(Operacion.multMatrizRecPar(m1, m2) == Vector(Vector(19, 22), Vector(43, 50)))
+    }
+    test("testTaller4 II") {
+        val m1 = Vector(Vector(1,0,1,0),Vector(0,1,0,1),Vector(1,0,1,0),Vector(0,1,0,1))
+        val m2 = Vector(Vector(0,1,0,1),Vector(1,0,1,0),Vector(0,1,0,1),Vector(1,0,1,0))
+        assert(Operacion.multMatriz(m1, m2) == Vector(Vector(0,2,0,2),Vector(2,0,2,0),Vector(0,2,0,2),Vector(2,0,2,0)))
+        assert(Operacion.multMatrizPar(m1, m2) == Vector(Vector(0,2,0,2),Vector(2,0,2,0),Vector(0,2,0,2),Vector(2,0,2,0)))
+        assert(Operacion.multMatrizRec(m1, m2) == Vector(Vector(0,2,0,2),Vector(2,0,2,0),Vector(0,2,0,2),Vector(2,0,2,0)))
+        assert(Operacion.multMatrizRecPar(m1, m2) == Vector(Vector(0,2,0,2),Vector(2,0,2,0),Vector(0,2,0,2),Vector(2,0,2,0)))
+        assert(Operacion.multStrassen(m1, m2) == Vector(Vector(0,2,0,2),Vector(2,0,2,0),Vector(0,2,0,2),Vector(2,0,2,0)))
+        assert(Operacion.multStrassenPar(m1, m2) == Vector(Vector(0,2,0,2),Vector(2,0,2,0),Vector(0,2,0,2),Vector(2,0,2,0)))
+
+    }
+    test("Multiplicacion Estandar de Matrices"){
+        val m1 = Operacion.matrizAlAzar(2,2)
+        val m2 = Operacion.matrizAlAzar(2,2)
+        assert(Operacion.multMatriz(m1,m2) == Operacion.multMatrizPar(m1,m2))
+    }
+    test ("Multiplicacion Recursiva de Matrices"){
         val m1 = Operacion.matrizAlAzar(2, 2)
         val m2 = Operacion.matrizAlAzar(2, 2)
-        assert(Operacion.multMatriz(m1,m2) == Operacion.multMatrizPar(m1,m2))
+        assert(Operacion.multMatrizRec(m1,m2) == Operacion.multMatrizRecPar(m1,m2))
+    }
+    test ("Multiplicacion Metodo : Strassen De Matrices"){
+        val m1 = Operacion.matrizAlAzar(2, 2)
+        val m2 = Operacion.matrizAlAzar(2, 2)
+        assert(Operacion.multStrassen(m1,m2) == Operacion.multStrassenPar(m1,m2))
     }
 }
